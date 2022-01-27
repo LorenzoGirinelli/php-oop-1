604769ac6e5684e656aaca23684cc65f05a26171
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-01-27 16:54:36 +0100

    finito

--- index.php
+++ index.php
@@ -11,5 +11,32 @@
         public function getVote() {
             return $this->vote = $_vote; 
         }
+
+        public function generateRandomVote() {
+            $randomVote = rand(0, 10);
+            $this->vote = $randomVote;
+        }
     }
+
+    $Fast_and_Furious_10 = new Movie('Fast & Furious 10', 8.5);
+    $Scream = new Movie('Scream',7.0);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie</title>
+</head>
+<body>
+    <div>
+        Titolo: <?php echo $Fast_and_Furious_10->title ?> - Vote: <?php echo $Fast_and_Furious_10->vote ?>
+    </div>
+
+    <div>
+        Titolo: <?php echo $Scream->title ?> - Vote: <?php echo $Scream->vote ?>
+    </div>
+</body>
+</html>
